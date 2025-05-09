3be3fe2e00df01a28bcba12add13048a12553513
JasonWachira <jason.kamwaro@strathmore.edu>
2025-05-09 12:12:10 +0300

    Fourth commmit

--- Linked-List.py
+++ Linked-List.py
@@ -42,4 +42,56 @@ class LinkedList:
         new_node = Node(data)
         current_node.next = new_node
 
+    def delete_head(self):
+        if self.head is None:
+            print("Linked List is empty")
+            return
+
+        current_node = self.head
+        self.head = current_node.next
+
+    def delete_at_index(self, index):
+        if self.head is None:
+            print("Linked List is empty")
+            return
+
+        if index == 0:
+            self.delete_head()
+            return
+
+        current_node = self.head
+        position = 0
+
+        while current_node is not None and position < index - 1:
+            position += 1
+            current_node = current_node.next
+
+        if current_node is None or current_node.next is None:
+            print("List empty at index")
+            return
+        current_node.next = current_node.next.next
+
+    def delete_last_node(self):
+        if self.head is None:
+            print("Linked List is empty")
+            return
+
+        current_node = self.head
+        while current_node.next is not None and current_node.next.next is not None:
+            current_node = current_node.next
+
+        current_node.next = None
+
+    def search(self, data):
+        current_node = self.head
+
+        while current_node.next is not None:
+            if current_node.data == data:
+                return current_node
+            else:
+                current_node = current_node.next
+
+        print("Item not found")
+
+
 
