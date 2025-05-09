51c639d7007979ff5bbb427e5a1bf558cec534a3
JasonWachira <jason.kamwaro@strathmore.edu>
2025-05-09 11:43:13 +0300

    third commit

--- Linked-List.py
+++ Linked-List.py
@@ -1,8 +1,8 @@
-
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
 
 class LinkedList:
     def __init__(self):
@@ -24,9 +24,22 @@ class LinkedList:
         current_node = self.head
         position = 0
 
+        while current_node is not None and position + 1 != index:
+            position += 1
+            current_node = current_node.next
 
+        if current_node is not None:
+            new_node = Node(data)
+            new_node.next = current_node.next
+            current_node.next = new_node
 
+    def Insert_Node_At_End(self, data):
+        current_node = self.head
 
+        while current_node.next is not None:
+            current_node = current_node.next
 
+        new_node = Node(data)
+        current_node.next = new_node
 
 
